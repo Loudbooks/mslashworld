8a60d07981ff53d5d913bf6b04671f9fce5b786b
Loudbook <loudbooked@gmail.com>
2023-07-30 14:06:50 -0400

    initial commit

---
+++ src/main/kotlin/com/loudbook/dev/MSlashWorld.kt
@@ -0,0 +1,11 @@
+package com.loudbook.dev
+
+import net.minestom.server.MinecraftServer
+
+class MSlashWorld {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+        }
+    }
+}
